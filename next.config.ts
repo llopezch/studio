7a1775ac2a8539002f8fbd05c94aefdc1a9224d0
Luis Lopez <LUISALBERTT22@gmail.com>
2025-07-26 22:34:55 +0000

    HAY ERRORES EN EL CODIGO EN ROJO

--- next.config.ts
+++ next.config.ts
@@ -14,10 +14,6 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
       },
-      {
-        protocol: 'https',
-        hostname: 'www.bn.com.pe',
-      },
     ],
   },
 };
